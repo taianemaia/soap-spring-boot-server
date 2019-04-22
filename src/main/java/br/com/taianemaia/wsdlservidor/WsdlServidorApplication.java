1b670fd684fdf1bb141cc8173464015588c3debe
Taiane Maia <taianemaia@outlook.com>
2019-04-22 11:25:46 -0300

    Initial commit

---
+++ src/main/java/br/com/taianemaia/wsdlservidor/WsdlServidorApplication.java
@@ -0,0 +1,13 @@
+package br.com.taianemaia.wsdlservidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlServidorApplication.class, args);
+	}
+
+}
